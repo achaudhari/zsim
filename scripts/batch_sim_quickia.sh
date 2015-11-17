b8a83a721c2976a75fb318c41f617c105bc66334
Ashish Chaudhari <achaudha@stanford.edu>
2015-11-17 00:05:41 -0800

    Updated batch simulation script

--- scripts/batch_sim_quickia.sh
+++ scripts/batch_sim_quickia.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-declare -a benchmarks=("blackscholes-sm" "fluidanimate-sm" "vips-sm")
-declare -a schedulers=("bb" "ss")
+declare -a benchmarks=("blackscholes-sm" "fluidanimate-sm" "facesim-sm" "swaptions-sm" "x264-sm" "vips-sm" "raytrace" "bodytrack-sm" "freqmine-sm")
+declare -a schedulers=("1b" "1s" "bb" "ss" "bs" "fair")
 
 logfile=quickia_log_`date +"%Y-%m-%d_%H%M"`.csv
 rm -f $logfile
@@ -11,6 +11,6 @@ do
     for s in "${schedulers[@]}"
     do
         echo "INFO: Running benchmark $b with scheduler $s..."
-        ./simulate_quickia.py --hp_tasks=$b --lp_tasks=$b --scheduler=$s --log=$logfile run
+        ./simulate_quickia.py --hp_tasks=$b --scheduler=$s --log=$logfile --instr_window=10000000-210000000 run
     done
 done
